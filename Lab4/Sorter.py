2f4cd5389605f0821b988be73154b233d1052740
JesseFentress <jessefentress246@gmail.com>
2022-02-18 19:59:52 -0500

    setting it up

--- Lab4/Sorter.py
+++ Lab4/Sorter.py
@@ -1,6 +1,12 @@
 import sys
 
 class Sorter:
+    def __init__(self):
+        s_size = 0
+        i_size = 0
+        b_size = 0
+        m_size = 0
+        q_size = 0
 
     def selection_sort(self, l, key):
         for item in range(len(l)):
@@ -9,6 +15,7 @@ class Sorter:
                 if l[item2][key] < l[smallest][key]:
                     smallest = item2
             l[item], l[smallest] = l[smallest], l[item]
+        print("Selection Sort Size " + str(key) + " : " + str(sys.getsizeof(l) + sys.getsizeof(key) + sys.getsizeof(smallest)))
         return l
 
     def insertion_sort(self, l, key):
@@ -17,9 +24,7 @@ class Sorter:
             while l[item - 1][key] > value and item > 0:
                 l[item], l[item - 1] = l[item - 1], l[item]
                 item = item - 1
-            print("ins (value)" + str(sys.getsizeof(value)))
-        print("ins (l)" + str(sys.getsizeof(l)))
-        print("ins (key)" + str(sys.getsizeof(key)))
+        print("Insertion Sort Size " + str(key) + " : " + str(sys.getsizeof(l) + sys.getsizeof(key) + sys.getsizeof(value) + sys.getsizeof(item)))
         return l
 
 
@@ -28,8 +33,7 @@ class Sorter:
             for item2 in range(len(l) - 1 - item):
                 if l[item2][key] > l[item2 + 1][key]:
                     l[item2], l[item2 + 1] = l[item2 + 1], l[item2]
-        print("bubble (l)" + str(sys.getsizeof(l)))
-        print("bubble (key)" + str(sys.getsizeof(key)))
+        print("Bubble Sort Size " + str(key) + " : " + str(sys.getsizeof(l) + sys.getsizeof(key)))
         return l
 
     def merge_sort(self, l, r, key):
@@ -44,29 +48,22 @@ class Sorter:
                 right += 1
         temp.extend(l[left:])
         temp.extend(r[right:])
-        print("ms (l)" + str(sys.getsizeof(l)))
-        print("ms (r)" + str(sys.getsizeof(l)))
-        print("ms (key)" + str(sys.getsizeof(l)))
-        print("ms (temp)" + str(sys.getsizeof(temp)))
         return temp      
 
     def merge(self, l, key):
         if len(l) == 1:
             return l
-        print("merge (l)" + str(sys.getsizeof(l)))
-        print("merge (key)" + str(sys.getsizeof(key)))
         return self.merge_sort(self.merge(l[:(len(l) // 2)], key), self.merge((l[(len(l) // 2):]), key), key)
 
-    def swap(self, l, a, b):
+    def swap(self, l, a, b, sum):
         temp = l[a]
         l[a] = l[b]
         l[b] = temp
-        print("swap (l)" + str(sys.getsizeof(l)))
-        print("swap (a)" + str(sys.getsizeof(a)))
-        print("swap (b)" + str(sys.getsizeof(b)))
-        print("swap (temp)" + str(sys.getsizeof(temp)))
+        sum = sum + sys.getsizeof(l) + sys.getsizeof(a) + sys.getsizeof(b) + sys.getsizeof(temp)
+        return sum
+        
 
-    def partition(self, l, left, high, key):
+    def partition(self, l, left, high, key, sum):
         pivot = l[high][key]
         right = high
         while left < right:
@@ -74,27 +71,19 @@ class Sorter:
                 left = left + 1
             while l[right][key] >= pivot and left < right:
                 right = right - 1
-            self.swap(l, left, right)
-        self.swap(l, left, high)
-        print("part (l)" + str(sys.getsizeof(l)))
-        print("part (left)" + str(sys.getsizeof(left)))
-        print("part (high)" + str(sys.getsizeof(high)))
-        print("part (key)" + str(sys.getsizeof(key)))
-        print("part (right)" + str(sys.getsizeof(right)))
-        print("part (pivot)" + str(sys.getsizeof(pivot)))
+            sum = sum + self.swap(l, left, right, sum)
+        sum = sum + self.swap(l, left, high, sum)
+        print(sum)
         return left
 
-    def quick_sort(self, l, low, high, key):
+    def quick_sort(self, l, low, high, key, sum):
         if low > high:
             return
-        print("qs (l)" + str(sys.getsizeof(l)))
-        print("qs (low)" + str(sys.getsizeof(low)))
-        print("qs (high)" + str(sys.getsizeof(high)))
-        print("qs (key)" + str(sys.getsizeof(key)))
-        pivot = self.partition(l, low, high, key)
-        print("qs (pivot)" + str(sys.getsizeof(pivot)))
-        self.quick_sort(l, low, pivot - 1, key)
-        self.quick_sort(l, pivot + 1, high, key)
+        pivot = self.partition(l, low, high, key, sum)
+        sum = sum + sys.getsizeof(pivot) + sys.getsizeof(high) + sys.getsizeof(low) + sys.getsizeof(l) + sys.getsizeof(key)
+        self.quick_sort(l, low, pivot - 1, key, sum)
+        self.quick_sort(l, pivot + 1, high, key, sum)
+        return sum
         
 
 
