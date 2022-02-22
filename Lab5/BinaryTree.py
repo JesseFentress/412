11725f7cc21e6126b2164e88e6ab547b49e9b5fd
JesseFentress <jessefentress246@gmail.com>
2022-02-22 10:57:55 -0500

    starting

---
+++ Lab5/BinaryTree.py
@@ -0,0 +1,31 @@
+class BinaryTree:
+
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.left = None
+            self.right = None
+
+    def __init__(self):
+        pass
+
+    def insert(self, data):
+        pass
+
+    def find(self, data):
+        pass
+
+    def min(self):
+        pass
+
+    def max(self):
+        pass
+
+    def inorder(self):
+        pass
+
+    def preorder(self):
+        pass
+
+    def postorder(self):
+        pass
